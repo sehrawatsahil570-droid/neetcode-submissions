0ee3be22e224d39e1de02fa4ef08229150da5ef4
sehrawatsahil570-droid <sehrawatsahil570@gmail.com>
2026-09-19 15:28:25 +0530

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minprice=prices[0];
+        
+        int maxprofit=0;
+        for(int i=0;i<prices.size();i++){
+            int profit= prices[i]-minprice;
+            maxprofit=max(profit,maxprofit);
+            minprice=min(prices[i],minprice);
+           
+            
+        }
+        return maxprofit;
+    }
+};
