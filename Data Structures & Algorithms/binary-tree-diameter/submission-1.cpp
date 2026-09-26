5244f9d5544ba961f407501b4d32274beed0421d
sehrawatsahil570-droid <sehrawatsahil570@gmail.com>
2026-09-27 00:35:01 +0530

    Add: binary-tree-diameter - submission-1

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    int height(TreeNode* root, int &diameter) {
+
+        if (root == nullptr)
+            return 0;
+
+        int left = height(root->left, diameter);
+        int right = height(root->right, diameter);
+
+        diameter = max(diameter, left + right);
+
+        return 1 + max(left, right);
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+
+        int diameter = 0;
+
+        height(root, diameter);
+
+        return diameter;
+    }
+};
